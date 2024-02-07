f59d0048a6ff18d32a1d1043d6ec2074cd789abf
Povo43 <140213985+Povo43@users.noreply.github.com>
2024-02-08 06:23:15 +0900

    dafeesdfs

---
+++ pages/login_signin/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+ <head>
+ <meta charset="utf-8">
+ <title>ログイン - BookTower</title>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="stylesheet" href="login_signin.css">
+ <link rel="stylesheet" href="../../others/css/style.css">
+ <script src="main.js"></script>
+ </head>
+ <body>
+        <header>
+			<div>
+				<a href="index.html">
+				<img src="../../others/images/BOOKTOWER1.png" alt="logo" class="logo"></a>
+			</div>
+
+		</header>
+ <!----- main ----->
+ <article>
+ </article>
+ <!----- /main ----->
+ 
+ <!----- footer ----->
+ <footer>フッター</footer>
+ <!----- /footer ----->
+ </body>
+</html>
