bd963237e2a781049a9169670618c872d7bd9a43
Povo43 <140213985+Povo43@users.noreply.github.com>
2024-03-03 06:51:22 +0900

    jjhh

--- pages/login_signin/login.php
+++ pages/login_signin/login.php
@@ -4,25 +4,33 @@
  <meta charset="utf-8">
  <title>ログイン - BookTower</title>
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <link rel="stylesheet" href="login_signin.css">
  <link rel="stylesheet" href="../../others/css/style.css">
+ <link rel="stylesheet" href="./login_signin.css">
  <script src="main.js"></script>
  </head>
  <body>
-        <header>
-			<div>
-				<a href="index.html">
-				<img src="../../others/images/BOOKTOWER1.png" alt="logo" class="logo"></a>
-			</div>
+    <header>
+		<div>
+			<a href="index.html">
+			<img src="../../others/images/BOOKTOWER1.png" alt="logo" class="logo"></a>
+		</div>
+	</header>
 
-		</header>
- <!----- main ----->
- <article>
- </article>
- <!----- /main ----->
- 
- <!----- footer ----->
- <footer>フッター</footer>
- <!----- /footer ----->
+	<div class="forms">
+		<form action="" method="POST">
+   		    <label for="signin-id">アカウント名</label>
+    	    <input id="signin-id" name="username" type="text" placeholder="メールアドレスを入力">
+    	    <label for="signin-pass">パスワード</label>
+    	    <input id="signin-pass" name="password" type="text" placeholder="パスワードを入力">
+    	    <button name="signin" type="submit">ログインする</button>
+    	</form>
+	</div>
+
+	<footer>
+		<div class="footer">
+			<p><small>Copyright &copy; 2024 Povo Tako All Rights Reserved.</small></p>
+			<a href="" class="footer-a">プライバシーポリシー</a>
+		</div>
+ 	</footer>
  </body>
 </html>
