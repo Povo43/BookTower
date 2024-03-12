a753561f6f986ca21bb607153d4aedd0e34f10e8
Povo43 <140213985+Povo43@users.noreply.github.com>
2024-03-13 07:00:49 +0900

    gfhfdgh

--- pages/login_signin/login.php
+++ pages/login_signin/login.php
@@ -4,9 +4,12 @@
  <meta charset="utf-8">
  <title>ログイン - BookTower</title>
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+ 	<script src="https://apis.google.com/js/platform.js" async defer></script>
+	<meta name="google-signin-client_id" content="412612120875-nr0bkf2nn3vtsmvb35d3ve97ab4q8e2j.apps.googleusercontent.com">
+
  <link rel="stylesheet" href="../../others/css/style.css">
  <link rel="stylesheet" href="./login_signin.css">
- <script src="main.js"></script>
  </head>
  <body>
     <header>
@@ -18,12 +21,10 @@
 
 
 	<div class="forms">
+		<div id="login_title">BookTower-ログイン</div>
 		<form action="" method="POST">
-   		    <label class="label1" for="signin-id">アカウント名</label>
-    	    <input id="signin-id" name="username" type="text" placeholder="メールアドレスを入力">
-			
-			<label class="label2" for="signin-pass">パスワード</label>
-    	    <input id="signin-pass" name="password" type="text" placeholder="パスワードを入力">
+	   	    <input id="signin-id" name="username" type="text" placeholder="ユーザー名 または メールアドレス">
+    	    <input id="signin-pass" name="password" type="text" placeholder="パスワード">
     	    <button class="form-button" name="signin" type="submit">ログインする</button>
     	</form>
 	</div>
