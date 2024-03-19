bac35faa02f1110d55a35d82e20665b80a45b978
Povo43 <140213985+Povo43@users.noreply.github.com>
2024-03-19 14:45:59 +0900

    argAXCdgeaw

--- pages/bookshelf/bookshelf.php
+++ pages/bookshelf/bookshelf.php
@@ -12,10 +12,25 @@
 		<header>
 			<a href="../../index.html" class="header-logo"><img src="../../others/images/BOOKTOWER1.png" alt="logo" class="logo"></a>
 			<form class="search">
-				<input id="title_serch" name="book_shelf" type="text" placeholder="本棚または本を検索">
+				<!--検索対象ラジオボタン-->
+				<input type="radio" id="bookshelf" name="book_or_shelf_radio" value="bookshelf">
+				<label class="ra1_img ra_img" for="bookshelf">
+			  		<img src="../../others/images/bookshelf/bookshelf_button.png">
+				</label>
+				
+				<input type="radio" id="book" name="book_or_shelf_radio" value="book" checked>
+				<label class="ra2_img ra_img" for="book">
+			  		<img src="../../others/images/bookshelf/book_button.png">
+				</label>
+				<!--入力欄-->
+				<label>
+        		<input type="text" placeholder="キーワードを入力">
+    			</label>
+    			<button type="submit" aria-label="検索" class="search_button"></button>
 			</form>
 		</header>
 
+		<!--新規の本を登録するボタン-->
 		<a class="new" href="../new/new.php"><div class="new__arrow"></div></a>
 
 		<div class="tab">
